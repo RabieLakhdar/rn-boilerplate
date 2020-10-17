4dc25e5dcbc34ba15e17264a48b6337baa24df91
rabieLakhdar <rlakhdar@maltem.com>
2020-10-17 12:49:12 +0100

    I18N

--- src/scenes/login/index.js
+++ src/scenes/login/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import AreaView from 'components/atoms/AreaView';
 import Title from 'components/atoms/Title';
 import Button from 'components/atoms/Button';
+import I18n from 'i18n';
 
 const LoginScreen = ({navigation}) => (
   <AreaView>
-    <Title>Screen: Login</Title>
-
+    <Title>{`${I18n.t('greeting')} Screen: Login`}</Title>
     <Button onPress={() => navigation.navigate('Home')}>
       <Title>Go to home</Title>
     </Button>
